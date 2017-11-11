793b1c2dd611989c9d1eaace61f510c23b6d3129
KingEridani <33461616+KingEridani@users.noreply.github.com>
2017-11-11 11:42:38 -0800

    Update slave.js

--- slave.js
+++ slave.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Slave') {
+    	message.reply('Did you call for me Master?');
   	}
 });
 
