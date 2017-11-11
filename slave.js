833a3780e0509f3925a41b04f2a9beb46300aeb1
KingEridani <33461616+KingEridani@users.noreply.github.com>
2017-11-11 13:44:17 -0800

    Update slave.js

--- slave.js
+++ slave.js
@@ -9,7 +9,8 @@ client.on('message', message => {
     if (message.content === 'Slave') {
     	message.reply('Did you call for me Master?');
   	}
-    
+});
+
 client.on('message', message => {
     if (message.content === 'slave') {
     	message.reply('Did you call for me Master?');
