5506694b13e315af09af0f3f62a8642430d2d768
KingEridani <33461616+KingEridani@users.noreply.github.com>
2017-11-11 16:54:37 -0800

    Update slave.js

--- slave.js
+++ slave.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'fuck') {
-    	message.send('there there Master.');
+    	message.reply('there there Master.');
   	}
 });
 
